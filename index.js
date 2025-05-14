4ac7b0bf29000ffb37bfd710d6b81c9e7e4cb752
dnzcsgn <dnzcsgndc@gmail.com>
2025-05-14 16:37:32 -0700

    updated

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+  let isLocked = false;
 
+  const loginAttempt = (passwordAttempt) => {
+      if (isLocked) {
+          return "Account locked due to too many failed login attempts";
+      }
 
+      attemptCount++;
+
+      if (passwordAttempt === userInfo.password) {
+          return "Login successful";
+      }
+
+      if (attemptCount >= 3) {
+          isLocked = true;
+          return "Attempt 3: Login failed";
+      }
+
+      return `Attempt ${attemptCount}: Login failed`;
+  };
+
+  return loginAttempt;
+}
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
